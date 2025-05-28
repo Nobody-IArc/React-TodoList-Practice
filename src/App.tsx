36757797bba36d013fd30273fc9cd0bda460e76d
Nobody-IArc <utk3970@naver.com>
2025-05-28 22:39:33 +0900

    다시 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import React from 'react';
+import './App.css';
+
 function App() {
+    // { } 를 이용해 데이터 바인딩
+    const title = "내일의 노동";
+
+    const divStyle : React.CSSProperties = {
+        backgroundColor: 'palegoldenrod',
+        color: 'aqua',
+        fontSize: '1.8em',
+        textAlign: 'center',
+    };
+
     // 바벨에 의해 JS 코드로 변환됨
     return (
-        <div>
-            <h1>Hello</h1>
-            <p>React</p>
+        <div className='title'>
+            {/* { } 를 이용해 데이터 바인딩 */}
+            <h1>{ title }</h1>
+            <div style={divStyle}>
+                <h2>{ title }</h2>
+            </div>
         </div>
     );
 }
