8dd6eb074d4c15ce7350be0fa46b8a2ab3b4c79d
Nobody-IArc <utk3970@naver.com>
2025-05-29 18:28:23 +0900

    상태 및 이벤트 관리

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -10,10 +10,20 @@ type Todo = {
 
 const TodoList: React.FC = () => {
     const [todos, setTodos] = useState<Todo[]>([
-        { id: 1, text: 'Git Upload', isChecked: false },
-        { id: 2, text: 'Coffee', isChecked: false },
-        { id: 3, text: 'Code', isChecked: false },
+        {id: 1, text: 'Git Upload', isChecked: false},
+        {id: 2, text: 'Coffee', isChecked: false},
+        {id: 3, text: 'Code', isChecked: false},
     ]);
+
+    const handleCheckBox = (itemId: number) => {
+        setTodos((prevItems) =>
+            prevItems.map((item) =>
+                item.id === itemId ? { ...item, isChecked : !item.isChecked } : item
+            )
+        );
+        console.log('CheckBox State Changed');
+    }
+
     return (
         <div>
             <h1 className='titlebk'>오늘의 노동</h1><p></p>
@@ -22,7 +32,16 @@ const TodoList: React.FC = () => {
                     <ul>
                         {
                             todos.map((todo: Todo) => (
-                                <li key={todo.id}>{ todo.text }</li>
+                                <li key={todo.id}>
+                                    <input
+                                        type='checkbox'
+                                        onChange={() => {
+                                            handleCheckBox(todo.id);
+                                        }}></input>
+                                    <span>{
+                                        todo.isChecked ? <del>{todo.text}</del> : <span>{todo.text}</span>
+                                    }</span>
+                                </li>
                             ))
                         }
                     </ul>
