9413e5a36d99e9a52973dce5f7690a805dc956d9
Nobody-IArc <utk3970@naver.com>
2025-05-29 17:20:21 +0900

    컴포넌트 활용 2

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,14 +1,33 @@
 import React from "react";
+import {useState} from "react";
 import './App.css';
 
-interface MyComponentProps {
-    title: string;
+type Todo = {
+    id: number;
+    text: string;
+    isChecked: boolean;
 }
 
-const TodoList: React.FC<MyComponentProps> = ({ title }) => {
-    return(
-        <div className="container">
-            <h1>{ title }</h1>
+const TodoList: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([
+        { id: 1, text: 'Git Upload', isChecked: false },
+        { id: 2, text: 'Coffee', isChecked: false },
+        { id: 3, text: 'Code', isChecked: false },
+    ]);
+    return (
+        <div>
+            <div className="container">
+                <h1 className='titlebk'>오늘의 노동</h1>
+                <div className='container'>
+                    <div className='board'>
+                        <ul>
+                            <li>{todos[0].text}</li>
+                            <li>{todos[1].text}</li>
+                            <li>{todos[2].text}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
